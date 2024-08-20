13abd15563de57243974af340ee057dcac906b0e
Lucas Rangel <lucas@rangel.dev>
2024-08-20 18:13:27 -0300

    Implement find and list in pokemon controller

--- api/src/pokemon/pokemon.controller.ts
+++ api/src/pokemon/pokemon.controller.ts
@@ -1,5 +1,33 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 
-@Controller('pokemon')
+function parsePokemon(pokemon: any) {
+  return {
+    name: pokemon.name,
+    cover: pokemon.sprites?.other?.dream_world?.front_default,
+  };
+}
+
+@Controller("pokemon")
 export class PokemonController {
+  baseURI = "https://pokeapi.co/api/v2";
+  perPage = 20;
+
+  @Get(":name")
+  async find(@Param() params: any): Promise<any> {
+    return await fetch(`${this.baseURI}/pokemon/${params.name}`)
+      .then((x) => x.json())
+      .then(parsePokemon);
+  }
+
+  @Get("all/:page")
+  async list(@Param("page") page: any): Promise<any> {
+    const offset = parseInt(page) * this.perPage;
+    const names = await fetch(
+      `${this.baseURI}/pokemon?limit=${this.perPage}&offset=${offset}`,
+    )
+      .then((x) => x.json())
+      .then(({ results }) => results.map(({ name }) => name));
+
+    return names;
+  }
 }
