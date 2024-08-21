5378bb012064a9ba8bcc4c59c5d26959b5d2f178
Lucas Rangel <lucas@rangel.dev>
2024-08-21 20:38:59 -0300

    Extract header component

--- app/src/app/pokedex-app.module.ts
+++ app/src/app/pokedex-app.module.ts
@@ -9,6 +9,7 @@ import { HttpClient } from '@angular/common/http';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { PokemonsViewComponent } from './pokemons-view/pokemons-view.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
@@ -20,6 +21,6 @@ import { PokemonsViewComponent } from './pokemons-view/pokemons-view.component';
   providers: [PokemonService, provideHttpClient(withFetch())],
   declarations: [PokedexAppComponent],
   bootstrap: [PokedexAppComponent],
-  exports: [PokemonCardComponent, PokemonsViewComponent],
+  exports: [PokemonCardComponent, PokemonsViewComponent, HeaderComponent],
 })
 export class PokedexAppModule {}
